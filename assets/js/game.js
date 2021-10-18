2458ea2a7bbfb93495b2803f0ff818abfb99b7ab
DSiedlarz90 <dsiedlarz90@gmail.com>
2021-10-18 14:59:56 -0400

    add round count and defeat message

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,12 +1,11 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
-
+var playerMoney = 10;
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
-var playerMoney = 10;
 
 
 var fight = function(enemyName) {
@@ -31,7 +30,7 @@ var fight = function(enemyName) {
         // remove enemy's health by subtracting the amount set in the playerAttack variable
         enemyHealth = enemyHealth - playerAttack;
         console.log(
-        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+            playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
         );
         // check enemy's health
         if (enemyHealth <= 0) {
@@ -56,12 +55,25 @@ var fight = function(enemyName) {
                 break;
             }   else {
                 window.alert(playerName + " still has " + playerHealth + " health left.");
-                }
+            }
     }
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(enemyNames[i]);
+    if (playerHealth > 0) {
+        //let player know what round they are in, remember arrays start at 0 so it needs to have 1 added to it
+        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ));
+        //pick new enemy to fight based on the index of the enemyNames array
+        var pickedEnemyName = enemyNames[i];
+        //reset enemy health before starting new fight
+        enemyHealth = 50;
+        //usedebugger to pause script from running and check whats going on at that moment in the code
+        //debugger;
+        //pass the pickedEnemyName variable's value into the fight funcaiton where it will assume the enemyName parameter
+        fight(pickedEnemyName);
+    }
+    else {
+        window.alert("You have lost your robot in battle! Game over!");
+        break;
+    }
 }
